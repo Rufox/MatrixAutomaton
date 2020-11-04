a944286e3a68f3cf7900f792ffa6984dd7e59bd7
Isidoracha <isidorach.a@hotmail.com>
2020-11-03 22:02:48 -0300

    Actualización funcion slurmCluster para soporte de Orca

--- Lanzador.py
+++ Lanzador.py
@@ -1,4 +1,5 @@
 import subprocess as sb
+import Var as var
 
 # Llamada a sistema cluster,
 def envioCluster(gaussian, nombre, file, proc, cola):
@@ -16,8 +17,14 @@ def slurmCluster(nombre, file, proc, cola):
     slrm.write("#SBATCH --nodes=1\n")
     slrm.write("#SBATCH -c "+proc+"\n")
     slrm.write("#SBATCH --output=/dev/null\n")
-    slrm.write("\nml g16/B.01\n\n")
-
-    slrm.write("\nsrun g16 "+str(file)+"\n")
+    if var.Big_variable["software"] == "orca":
+        slrm.write("\nml orca/B.01\n\n")
+        slrm.write("\nsrun orca "+str(file)+"\n")
+    elif var.Big_variable["software"] == "gaussian":
+        slrm.write("\nml g16/B.01\n\n")
+        slrm.write("\nsrun g16 "+str(file)+"\n")
+    else:
+        print("Programa no soportado")
     slrm.close()
     state = sb.Popen(["sbatch",nombre+".slrm"])
+
